ae96eb22ace889b2a59c06a61b18428120d1dd90
Armando Serrato <rsb987@hotmail.com>
2024-03-07 09:46:23 -0600

    Time: 149 ms (28.75%), Space: 33.6 MB (86.01%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.kt
@@ -0,0 +1,25 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun middleNode(head: ListNode?): ListNode? {
+        var list = mutableListOf<ListNode>()
+        var node = head;
+
+        while(node != null){
+            list.add(node)
+            node = node.next
+        }
+
+        var count = list.count()
+
+        return list[count / 2];
+    }
+}
